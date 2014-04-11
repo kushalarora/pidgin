15cdc8a7b34994dd5f648b0cb9a709136e93501a
vagrant <vagrant@vagrant.vm>
2014-04-11 23:09:54 +0000

    load file changed for dsr server

--- freebase/load_freebase.sh
+++ freebase/load_freebase.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-isql 3030 dba dba exec="ld_dir_all('/home/ubuntu/20140302/sieved', '*.gz', 'http://freebase.org');" &
+isql 3030 dba dba exec="ld_dir_all('/home/vagrant/f', '*.gz', 'http://freebase.org');" &
+isql 3030 dba dba exec="rdf_loader_run (log_enable=>3);" &
+isql 3030 dba dba exec="rdf_loader_run (log_enable=>3);" &
+isql 3030 dba dba exec="rdf_loader_run (log_enable=>3);" &
+isql 3030 dba dba exec="rdf_loader_run (log_enable=>3);" &
+isql 3030 dba dba exec="rdf_loader_run (log_enable=>3);" &
+isql 3030 dba dba exec="rdf_loader_run (log_enable=>3);" &
+isql 3030 dba dba exec="rdf_loader_run (log_enable=>3);" &
+isql 3030 dba dba exec="rdf_loader_run (log_enable=>3);" &
 isql 3030 dba dba exec="rdf_loader_run (log_enable=>3);" &
 isql 3030 dba dba exec="rdf_loader_run (log_enable=>3);" &
 isql 3030 dba dba exec="rdf_loader_run (log_enable=>3);" &
